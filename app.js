407e24cb58eab77f8a1c1dfad3f2ae890fe926a4
rt18070907 <rt18070907@gmail.com>
2025-05-10 23:45:51 +0500

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-let myBox = document.getElementById('calculator');
+const display = document.getElementById('display');
+function appendToDisplay(input) {
+        display.value += input;
+}
+function clearDisplay() {
+        display.value = '';
+}
+function calculateResult() {
+        try {
+                display.value = eval(display.value);
+        } catch (error) {
+                display.value = 'Error!';
+        }
+}
